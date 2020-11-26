f2754129bfa250d8ecb5cac7e3f6ecbe11ece086
Opilane TTHK <opilane@tthk.ee>
2020-11-26 13:26:31 +0200

    first commit

---
+++ Car/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        class Car
+        {
+            string name;
+            string model;
+            string registrationnumber;
+            string color;
+            int odometer;
+            int fueltank;
+
+            public Car(string _name, string _model, string _registrationnumber, string _color)
+            {
+                name = _name;
+                model = _model;
+                registrationnumber = _registrationnumber;
+                color = _color;
+                odometer = 0;
+                fueltank = 60;
+                Console.WriteLine($"the car {name} has been created.");
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Model
+            {
+               get { return model; }
+            }
+
+            public string  Registrationnumber
+            {
+                get { return registrationnumber; }
+                set
+                {
+                    if(value.Length == 6)
+                    {
+                        Registrationnumber = value;
+
+                    }else
+                    {
+                        registrationnumber = " Fast and speedrun";
+                    }
+                }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public int Odometer
+            {
+                get { return odometer; }
+            }
+
+            public int FuelTank
+            {
+                get { return fueltank; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car newCar = new Car("Audi s6", "sedan", "AMG801", "Green");
+            Console.WriteLine(newCar.Name);
+            Console.WriteLine(newCar.Model);
+            Console.WriteLine(newCar.Registrationnumber);
+            Console.WriteLine(newCar.Color);
+            Console.WriteLine(newCar.Odometer);
+            Console.WriteLine(newCar.FuelTank);
+
+        }
+    }
+}
---
+++ OPChallenges/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace OPChallenges
+{
+    class Program
+    {
+        class Dog
+        {
+             string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"the dog {name} has been created.");
+
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void Barks()
+            {
+                Console.WriteLine("Woof-woof, i'm going to eat you alive");
+                happiness += 0.2;
+            }
+
+            public void Wiggle()
+            {
+                Console.WriteLine("Wiggle-wiggle, its my life");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+
+            }
+
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}; Spots: {spots}; Happy: {happiness};");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Dog newDog = new Dog("Tommy", 10);
+            
+            
+            Console.WriteLine($"the level of happiness: {newDog.Happiness}");
+
+            while(newDog.Happiness != 1)
+            {
+                newDog.Barks();
+            }
+
+            newDog.Wiggle();
+
+            Console.WriteLine("Name the dog;");
+            string newDog = Console.ReadLine();
+            newDog.Rename(newDogName);
+            newDog.ShowDogData();
+
+        }
+    }
+}
